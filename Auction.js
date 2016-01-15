d06ef389958a782d9e8a66f413ca391d13969f61
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2016-01-15 04:59:45 -0500

    first

---
+++ Auction.js
@@ -0,0 +1,22 @@
+"use strict";
+class Auction {
+
+  constructor(data) {
+    this.data = data;
+  }
+
+  /**
+   * getUniqueBidders
+   * Returns the DUNS numbers for each unique bidder in the auction
+   **/
+  getUniqueBidders() {
+    var a = [];
+    var bids = this.data.bids;
+    for (var i=0, l=bids.length; i<l; i++)
+        if (a.indexOf(bids[i].bidder.duns_number) === -1 && bids[i].duns_number !== '')
+            a.push(bids[i].bidder.duns_number);
+    return a;
+  }
+}
+
+module.exports = Auction;
---
+++ test.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect;
+var fs = require('fs');
+var Auction = require('./Auction');
+var data = JSON.parse(fs.readFileSync('testData.json')).auction;
+
+describe("Testing Auction", function(){
+  describe("constructor", function (){
+    it("should create an Auction object", function (){
+      var a = new Auction(data);
+      expect(a.data).to.equal(data);
+    })
+  })
+
+  describe("uniqueBidders", function (){
+    it("test auction should have 2 unique bidders", function (){
+      var auction = new Auction(data);
+      expect(auction.getUniqueBidders().length).to.equal(2)
+    })
+  })
+
+})
